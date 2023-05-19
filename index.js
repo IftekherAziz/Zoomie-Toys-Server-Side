3497e10231ebaa9784d09028660805a3c2d0ca1c
Iftekher Aziz <azizchy02@gmail.com>
2023-05-19 12:31:10 +0600

    setup basic server

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+
+require('dotenv').config()
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+// Middleware:
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+    res.send("Zoomie Toys is running");
+})
+
+app.listen(port, () => {
+    console.log(`Zoomie Toys server is running on port: ${port}`);
+})
